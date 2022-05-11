3ae96751961d45b4c6f63028c3de9d1f2916a2bb
karkina <nico.soumare@gmail.com>
2022-05-11 09:11:11 +0200

    :bomb: first commit

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -12,17 +12,35 @@ import Json.Decode as Decode
 
 {-| Got from JS side, and Model to modify -}
 type alias Flags = { now : Int }
+
+type Direction = Up
+  | Down
+  | Left
+  | Right 
+type alias Snake = { positions : List Int,
+                    direction : Direction
+                  }
+
+snakeDepartInit : Model -> Model
+snakeDepartInit model =
+  let snake = {positions= [250],direction = Up} in
+  {model | player=snake}
+
 type alias Model =
   { gameStarted : Bool
   , lastUpdate : Int
   , time : Int
   , coloredSquare : Int
+  , cellsBoard : List (Html Msg)
+  , sizeBoard : Int
+  , player : Snake
   }
 
 init : Flags -> ( Model, Cmd Msg )
 init { now } =
   now
-  |> \time -> Model False time time 0
+  |> \time -> Model False time time 0 [] 40 (Snake [1500,50,150,250] Up)
+  |> gameCase 
   |> Update.none
 
 {-| All your messages should go there -}
@@ -30,34 +48,87 @@ type Key = ArrowUp | ArrowRight | ArrowDown | ArrowLeft | Space
 type Msg
   = NextFrame Posix
   | ToggleGameLoop
+  | KeyMovement Direction
   | KeyDown Key
 
 {-| Manage all your updates here, from the main update function to each
  -|   subfunction. You can use the helpers in Update.elm to help construct
  -|   Cmds. -}
+{-
 updateSquare : Model -> Model
-updateSquare ({ coloredSquare } as model) =
+updateSquare ({ coloredSquare} as model) =
   coloredSquare + 1
-  |> modBy 4
+  |> modBy 2
   |> Setters.setColoredSquareIn model
-
+-}
 toggleGameLoop : Model -> ( Model, Cmd Msg )
 toggleGameLoop ({ gameStarted } as model) =
   not gameStarted
   |> Setters.setGameStartedIn model
   |> Update.none
 
+
+directionSnake : Direction -> Model -> (Model,Cmd Msg)
+directionSnake directionClick model =
+  case (directionClick,model.player.direction) of
+    (Up,_) -> let snakeUpdate ={positions=model.player.positions,direction=Up} in
+             {model | player = snakeUpdate}
+             |> Update.none
+    (_,_) -> Update.none model
+
+
+movingSnake : Model -> Model
+movingSnake model =
+
+  movingSnakeHelp model (List.reverse(model.player.positions))
+
+movingSnakeHelp : Model -> List Int -> Model
+movingSnakeHelp model snakePositions=
+  case (model.player.direction,snakePositions) of
+    (Up,head::tail) -> let updateSnake = {positions = (head+40)::tail,direction=Up} in
+                       { model | player = updateSnake}
+    (_,head::tail) -> let updateSnake = {positions = (head+40)::tail,direction=Up} in
+                      { model | player = updateSnake}
+    (_,_) -> model
+
+updatePlateau : Model -> Model
+updatePlateau model =
+  updatePlateauHelp model model.player.positions (model.sizeBoard*model.sizeBoard)
+
+updatePlateauHelp : Model -> List Int -> Int -> Model
+updatePlateauHelp model player count=
+  case (model.cellsBoard,player,count) of
+  (_,_,0) -> model
+  ([],_,_) -> model
+  (_,[],_) -> model
+  (head::tail,headSnake::tailSnake,_) -> let cellColor = cell 0 model.coloredSquare in
+                                         let noCellColor = cell 1 model.coloredSquare in
+                                if headSnake == count then updatePlateauHelp ({model | cellsBoard = cellColor::model.cellsBoard}) tailSnake (count-1)
+                                else updatePlateauHelp ({model | cellsBoard = noCellColor::model.cellsBoard}) player (count-1)
+  
+
+
+
 keyDown : Key -> Model -> ( Model, Cmd Msg )
 keyDown key model =
   case Debug.log "key" key of
     Space -> update ToggleGameLoop model
-    _ -> Update.none model
+    ArrowUp -> update (KeyMovement Up) model
+    ArrowRight -> update (KeyMovement Right) model
+    ArrowDown -> update (KeyMovement Down) model
+    ArrowLeft -> update (KeyMovement Left) model
+
+
+   -- _ -> Update.none model
+
 
 nextFrame : Posix -> Model -> ( Model, Cmd Msg )
 nextFrame time model =
   let time_ = Time.posixToMillis time in
-  if time_ - model.lastUpdate >= 1000 then
-    updateSquare model
+  if time_ - model.lastUpdate >= 10000 then
+      -- updateSquare model
+    --|> movingSnake 
+    updatePlateau model
     |> Setters.setTime time_
     |> Setters.setLastUpdate time_
     |> Update.none
@@ -72,6 +143,7 @@ update msg model =
   case msg of
     ToggleGameLoop -> toggleGameLoop model
     KeyDown key -> keyDown key model
+    KeyMovement direction -> directionSnake direction model
     NextFrame time -> nextFrame time model
 
 {-| Manage all your view functions here. -}
@@ -80,16 +152,25 @@ cell index active =
   let class = if active == index then "cell active" else "cell" in
   Html.div [ Attributes.class class ] []
 
-movingSquare : Model -> Html msg
-movingSquare { coloredSquare } =
-  Html.div [ Attributes.class "grid" ]
-    [ cell 0 coloredSquare
-    , cell 1 coloredSquare
-    , cell 3 coloredSquare
-    , cell 2 coloredSquare
-    ]
+gameCase : Model -> Model
+gameCase model =
+   gameCaseHelper (model.sizeBoard*model.sizeBoard) model
 
-actualTime : Model -> Html msg
+gameCaseHelper : Int -> Model -> Model
+gameCaseHelper taille model = 
+  case taille of
+  0 -> model
+  nombre -> let cellGame = cell 1 model.coloredSquare in
+            gameCaseHelper (taille-1) {model | cellsBoard = cellGame::model.cellsBoard}
+
+  
+
+boardInit : Model -> Html Msg
+boardInit model =
+  Html.div[Attributes.class "grid"]
+    (model.cellsBoard)
+
+actualTime : Model -> Html Msg
 actualTime { time } =
   Html.div [ Attributes.class "actual-time" ]
     [ Html.text "Actual time"
@@ -118,7 +199,7 @@ view model =
   Html.main_ []
     [ Html.img [ Attributes.src "/logo.svg" ] []
     , explanations model
-    , movingSquare model
+    , boardInit model
     ]
 
 {-| Parts for the runtime. Get key presses and subscribe to
